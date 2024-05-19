d7367e24d4b31397346e160b76400825027f0b7d
joharasmus <joha.rasmus@gmail.com>
2024-05-20 00:22:31 +0200

    Separated out "players" into own container, plus adjusted a few nasty bugs, plus major speed improvements

--- MoreFairStats/Types.cs
+++ MoreFairStats/Types.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Cosmos;
 
 namespace MoreFairStats;
 
@@ -90,12 +91,11 @@ public class LadderRanker
     public bool Growing { get; set; }
 }
 
-public class PlayerStats
+public class Player
 {
-    public int AccountId { get; set; }
+    public string? id { get; set; }
     public string? UserName { get; set; }
     public int AHPoints { get; set; }
-    public List<RoundAppearance> RoundAppearances { get; set; } = [];
 }
 
 public class MoreFairData
@@ -104,6 +104,10 @@ public class MoreFairData
     private Container RoundDb { get; init; }
     private Container ConfigDb { get; init; }
     private Container RankersDb { get; init; }
+    private Container PlayersDb { get; init; }
+
+    public List<Player> PlayerList { get; init; } = new List<Player>();
+    public Dictionary<int, Player> PlayerDictionary { get; set; } = new Dictionary<int, Player>();
 
     public MoreFairData(string connectionString)
     {
@@ -115,6 +119,7 @@ public class MoreFairData
         RoundDb = cosmosDB.GetContainer("mfs-rounds");
         ConfigDb = cosmosDB.GetContainer("mfs-config");
         RankersDb = cosmosDB.GetContainer("mfs-ladderRankers");
+        PlayersDb = cosmosDB.GetContainer("mfs-players");
     }
 
     public async Task Create(LadderRanker ranker)
@@ -129,6 +134,18 @@ public class MoreFairData
         => await RankersDb.ReadItem<LadderRanker>($"R{round}L{ladder}P{accountId}", round);
     public async Task<RoundStats> GetRound(int round)
         => await RoundDb.ReadItem<RoundStats>(round.ToString(), round);
+    public async Task<Player> GetPlayer(int accountId)
+        => await PlayersDb.ReadItem<Player>(accountId.ToString(), accountId.ToString());
+
+    public async Task<FeedResponse<Player>> GetManyPlayers(IReadOnlyList<string> items)
+    {
+        var itemsWithPartitionKeys = new List<(string, PartitionKey)>();
+        foreach (var item in items)
+        {
+            itemsWithPartitionKeys.Add((item, new PartitionKey(item)));
+        }
+        return await PlayersDb.ReadManyItemsAsync<Player>(itemsWithPartitionKeys);
+    }
 
     public async Task Upsert(LadderStats ladderStats)
         => await LadderDb.UpsertItem(ladderStats, ladderStats.Round);
@@ -139,12 +156,31 @@ public class MoreFairData
     public async Task Upsert(LadderRanker ladderRanker)
         => await RankersDb.UpsertItem(ladderRanker, ladderRanker.Round);
 
+    public async Task Upsert(Player player)
+        => await PlayersDb.UpsertItem(player, player.id);
+
     public async Task PatchLadder(LadderStats ladderStats, IReadOnlyList<PatchOperation> patchOperations)
         => await LadderDb.PatchItem<LadderStats>(ladderStats.id!, ladderStats.Round, patchOperations);
 
     public FeedIterator<LadderStats> GetLadderQueryIterator(QueryDefinition queryDefinition)
-        => LadderDb.GetItemQueryIterator<LadderStats>(queryDefinition);
+        => LadderDb.GetItemQueryIterator<LadderStats>(queryDefinition, requestOptions: new() { MaxItemCount = -1} );
 
     public FeedIterator<LadderRanker> GetLadderRankersQueryIterator(QueryDefinition queryDefinition)
-        => RankersDb.GetItemQueryIterator<LadderRanker>(queryDefinition);
+        => RankersDb.GetItemQueryIterator<LadderRanker>(queryDefinition, requestOptions: new() { MaxItemCount = -1 } );
+
+    public FeedIterator<Player> GetPlayersQueryIterator(QueryDefinition queryDefinition)
+        => PlayersDb.GetItemQueryIterator<Player>(queryDefinition, requestOptions: new () { MaxItemCount = -1} );
+}
+
+public class PlayersRefreshService : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
--- MoreFairStatsManagement/Program.cs
+++ MoreFairStatsManagement/Program.cs
@@ -13,16 +13,17 @@ var moreFairData = new MoreFairData(dbConnStr);
 var mfsConfig = await moreFairData.GetConfig();
 var currentMaxRound = mfsConfig.CurrentMaxRound;
 
-for (int i = 1; i <= currentMaxRound; i++)
-{
-    var trialRound = await moreFairData.GetRound(i);
-    Console.WriteLine(trialRound.id);
-    for (int j = 1; j <= trialRound.NumberOfLadders; j++)
-    {
-        var trialLadder = await moreFairData.GetLadder(i, j);
-        await moreFairData.Upsert(trialLadder);
-    }
-}
+
+//for (int i = 1; i <= currentMaxRound; i++)
+//{
+//    var trialRound = await moreFairData.GetRound(i);
+//    Console.WriteLine(trialRound.id);
+//    for (int j = 1; j <= trialRound.NumberOfLadders; j++)
+//    {
+//        var trialLadder = await moreFairData.GetLadder(i, j);
+//        await moreFairData.Upsert(trialLadder);
+//    }
+//}
 
 async Task updateWithNewRoundStats(APIRoundStats apiRoundStats)
 {
@@ -34,7 +35,42 @@ async Task updateWithNewRoundStats(APIRoundStats apiRoundStats)
         var ladderStats = ladder.Value;
         ladderStats.Round = round;
         ladderStats.Ladder = int.Parse(ladder.Key);
+        Console.WriteLine(ladderStats.Ladder);
         ladderStats.id = $"R{round}L{ladder.Key}";
+
+        var tasks = new List<Task>();
+
+        foreach (var ranker in ladderStats.Rankers!)
+        {
+            var ladderRanker = new LadderRanker()
+            {
+                AccountId = ranker.AccountId,
+                AutoPromote = ranker.AutoPromote,
+                Bias = ranker.Bias,
+                Grapes = ranker.Grapes,
+                Growing = ranker.Growing,
+                id = $"{ladderStats.id}P{ranker.AccountId}",
+                Ladder = ladderStats.Ladder,
+                Multi = ranker.Multi,
+                Points = ranker.Points,
+                Power = ranker.Power,
+                Rank = ranker.Rank,
+                Round = round,
+                Vinegar = ranker.Vinegar
+            };
+            var player = new Player()
+            {
+                AHPoints = ranker.AssholePoints,
+                id = ranker.AccountId.ToString(),
+                UserName = ranker.UserName
+            };
+            tasks.Add(moreFairData.Upsert(ladderRanker));
+            tasks.Add(moreFairData.Upsert(player));
+        }
+        await Task.WhenAll(tasks);
+
+        ladderStats.Rankers = [];
+
         await moreFairData.Upsert(ladderStats);
     }
 }
@@ -52,110 +88,11 @@ async Task refreshLadderStatsForRound(APIRoundStats apiRoundStats)
     }
 }
 
-async Task UpdatePlayersWithNewNames(int roundToQuery)
-{
-    var firstLadder = await moreFairData.GetLadder(roundToQuery, 1);
-    var firstLadderRankers = firstLadder.Rankers!;
-    var queryString = $"SELECT * FROM c WHERE c.Round != {roundToQuery}";
-    var queryDef = new QueryDefinition(queryString);
-    using var queryIter = moreFairData.GetLadderQueryIterator(queryDef);
-    while (queryIter.HasMoreResults)
-    {
-        var result = await queryIter.ReadNextAsync();
-        foreach (var ladder in result)
-        {
-            Console.WriteLine(ladder.id);
-            var ladderRankers = ladder.Rankers!;
-            var index = 0;
-            foreach (var ranker in ladderRankers)
-            {
-                var matchRanker = firstLadderRankers.Find(r => r.AccountId == ranker.AccountId);
-                if (matchRanker != null && (matchRanker.UserName != ranker.UserName || matchRanker.AssholePoints != ranker.AssholePoints))
-                {
-                    Console.WriteLine(matchRanker.AccountId);
-                    var patchOps = new[]
-                    {
-                        PatchOperation.Replace($"/Rankers/{index}/UserName", matchRanker.UserName),
-                        PatchOperation.Replace($"/Rankers/{index}/AssholePoints", matchRanker.AssholePoints),
-                    };
-                    await moreFairData.PatchLadder(ladder, patchOps);
-                }
-                index++;
-            }
-        }
-    }
-}
-
 async Task UpdateNewRound()
 {
     var latestRoundData = await MoreFairAPI.GetRound(263);
     Console.WriteLine(latestRoundData.BasePointsToPromote);
     await updateWithNewRoundStats(latestRoundData);
-    await UpdatePlayersWithNewNames(latestRoundData.Number);
 }
 
-//await UpdateNewRound();
-
-// private void invertRoundsToPlayers()
-// {
-//     var allRankers = new SortedDictionary<int, PlayerStats>();
-//     for (int i = 1; i <= currentMaxRound; i++)
-//     {
-//         parseRoundStats(i);
-//         var firstLadder = roundStats!.Ladders!["1"];
-//         foreach (var ranker in firstLadder.Rankers!)
-//         {
-//             if (!allRankers.ContainsKey(ranker.AccountId))
-//             {
-//                 var player = new PlayerStats() { AccountId = ranker.AccountId, AHPoints = ranker.AssholePoints, UserName = ranker.UserName };
-//                 allRankers[ranker.AccountId] = player;
-//             }
-//             var roundApp = new RoundAppearance()
-//                 {
-//                     Ladders = [],
-//                     RoundTypes = roundStats.RoundTypes,
-//                     BasePointsToPromote = roundStats.BasePointsToPromote,
-//                     CreatedOn = roundStats.CreatedOn,
-//                     ClosedOn = roundStats.ClosedOn,
-//                     Number = roundStats.Number
-//                 };
-//             allRankers[ranker.AccountId].RoundAppearances!.Add(roundApp);
-//             foreach (var keyVal in roundStats.Ladders)
-//             {
-//                 var ladderNum = keyVal.Key;
-//                 var ladder = keyVal.Value;
-//                 var ladderRanker = ladder.Rankers!.Find(r => r.AccountId == ranker.AccountId);
-//                 var ladderApp = new LadderAppearance()
-//                     {
-//                         CreatedOn = ladder.CreatedOn,
-//                         BasePointsToPromote = ladder.BasePointsToPromote,
-//                         LadderTypes = ladder.LadderTypes,
-//                         Rank = ladderRanker!.Rank,
-//                         Points = ladderRanker.Points,
-//                         Power = ladderRanker.Power,
-//                         Multi = ladderRanker.Multi,
-//                         Bias = ladderRanker.Bias,
-//                         Grapes = ladderRanker.Grapes,
-//                         Vinegar = ladderRanker.Vinegar,
-//                         AutoPromote = ladderRanker.AutoPromote,
-//                         Growing = ladderRanker.Growing
-//                     };
-//                 roundApp.Ladders[ladderNum] = ladderApp;
-//                 if (ladderRanker.Growing) break;
-//             }
-
-//         }
-//         Console.WriteLine(i);
-//     }
-//     var tunaStats = allRankers[36780];
-//     foreach (var appearance in tunaStats.RoundAppearances)
-//     {
-//         Console.WriteLine(appearance.Number);
-//         Console.WriteLine(appearance.Ladders!.Values.Last().Vinegar);
-//     }
-//     var jsonString = JsonSerializer.Serialize(allRankers);
-//     Console.WriteLine("Serialized!");
-//     var fileName = "allRankers.json";
-//     var filePath = Environment.CurrentDirectory + $"\\Data\\{fileName}";
-//     File.WriteAllText(filePath, jsonString);
-// }
+await UpdateNewRound();
