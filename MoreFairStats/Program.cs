0df8873f0e3418cefcd59b9b287ff65e19ae8f4c
Rasmus Johansson <joha.rasmus@outlook.com>
2025-03-03 07:56:38 +0100

    remove exceptionhandler

--- MoreFairStats/Program.cs
+++ MoreFairStats/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using MoreFairStats;
 using MoreFairStats.Components;
 
@@ -10,16 +9,9 @@ var mfsCosmosDbConnStr = builder.Configuration["mfsCosmosDbConnStr"]!;
 var moreFairData = new MoreFairData(mfsCosmosDbConnStr);
 builder.Services.AddSingleton(moreFairData);
 
-// Add services to the container.
 builder.Services.AddRazorComponents();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-}
 
 app.UseStaticFiles();
 app.UseAntiforgery();
