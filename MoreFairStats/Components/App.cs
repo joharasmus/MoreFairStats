88cd5654eb06582a4682b874302e39d5e6737e01
Rasmus Johansson <joha.rasmus@outlook.com>
2025-03-03 06:14:01 +0100

    manually generate app.cs

---
+++ MoreFairStats/Components/App.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MoreFairStats.Components
+{
+    public partial class App : Microsoft.AspNetCore.Components.ComponentBase
+    {
+        protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
+        {
+            __builder.AddMarkupContent(0, "<!DOCTYPE html>\r\n");
+            __builder.OpenElement(1, "html");
+            __builder.AddAttribute(2, "lang", "en");
+            __builder.OpenElement(3, "head");
+            __builder.AddMarkupContent(4, @"<meta charset=""utf-8"">
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+    <base href=""/"">
+    <link rel=""stylesheet"" href=""app.css"">
+    <link rel=""stylesheet"" href=""MoreFairStats.styles.css"">
+    <link rel=""icon"" type=""image/png"" href=""favicon.png"">
+    ");
+            __builder.OpenComponent<Microsoft.AspNetCore.Components.Web.HeadOutlet>(5);
+            __builder.CloseComponent();
+            __builder.CloseElement();
+            __builder.AddMarkupContent(6, "\r\n\r\n");
+            __builder.OpenElement(7, "body");
+            __builder.OpenComponent<Routes>(8);
+            __builder.CloseComponent();
+            __builder.CloseElement();
+            __builder.CloseElement();
+        }
+    }
+}
