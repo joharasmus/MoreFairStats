91d42cb529aecf5cd7184cf2b2e081291de1d99e
Rasmus Johansson <joha.rasmus@outlook.com>
2025-03-03 07:04:39 +0100

    remove runtimehelpers, focusonnavigate and more

--- MoreFairStats/Components/Routes.cs
+++ MoreFairStats/Components/Routes.cs
@@ -1,6 +1,5 @@
 ﻿
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.CompilerServices;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.Routing;
 
@@ -11,16 +10,11 @@ public class Routes : ComponentBase
     protected override void BuildRenderTree(RenderTreeBuilder __builder)
     {
         __builder.OpenComponent<Router>(0);
-        __builder.AddComponentParameter(1, "AppAssembly", RuntimeHelpers.TypeCheck(typeof(Program).Assembly));
+        __builder.AddComponentParameter(1, "AppAssembly", typeof(Program).Assembly);
         __builder.AddAttribute(2, "Found", (RenderFragment<RouteData>)((routeData) => (__builder2) => {
             __builder2.OpenComponent<RouteView>(3);
-            __builder2.AddComponentParameter(4, "RouteData", RuntimeHelpers.TypeCheck(routeData));
-            __builder2.AddComponentParameter(5, "DefaultLayout", RuntimeHelpers.TypeCheck(typeof(MainLayout)));
-            __builder2.CloseComponent();
-            __builder2.AddMarkupContent(6, "\r\n        ");
-            __builder2.OpenComponent<FocusOnNavigate>(7);
-            __builder2.AddComponentParameter(8, "RouteData", RuntimeHelpers.TypeCheck(routeData));
-            __builder2.AddComponentParameter(9, "Selector", "h1");
+            __builder2.AddComponentParameter(4, "RouteData", routeData);
+            __builder2.AddComponentParameter(5, "DefaultLayout", typeof(MainLayout));
             __builder2.CloseComponent();
         }
         ));
