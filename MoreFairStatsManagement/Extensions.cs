2b9e9d9a3db84ace6ed11b6f5befc2dedf9b51aa
Rasmus Johansson <joha.rasmus@outlook.com>
2025-03-07 23:29:17 +0100

    Merge all files into management project

---
+++ MoreFairStatsManagement/Extensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure.Cosmos;
+using System.Text.Json;
+
+namespace MoreFairStats;
+
+public static class Extensions
+{
+    public static async Task CreateItem<T>(this Container container, T item, string partitionKey)
+    {
+        await container.CreateItemAsync(item, new PartitionKey(partitionKey));
+    }
+
+    public static async Task CreateItem<T>(this Container container, T item, int partitionKey)
+    {
+        await container.CreateItemAsync(item, new PartitionKey(partitionKey));
+    }
+
+    public static async Task<T> ReadItem<T>(this Container container, string id, int partitionKey)
+        => (await container.ReadItemAsync<T>(id, new PartitionKey(partitionKey))).Resource;
+
+    public static async Task<T> ReadItem<T>(this Container container, string id, string partitionKey)
+        => (await container.ReadItemAsync<T>(id, new PartitionKey(partitionKey))).Resource;
+
+    public static async Task UpsertItem<T>(this Container container, T item, int partitionKey)
+    {
+        await container.UpsertItemAsync(item, new PartitionKey(partitionKey));
+    }
+
+    public static async Task UpsertItem<T>(this Container container, T item, string partitionKey)
+    {
+        await container.UpsertItemAsync(item, new PartitionKey(partitionKey));
+    }
+
+    public static async Task PatchItem<T>(this Container container, string id, int partitionKey, IReadOnlyList<PatchOperation> patchOperations)
+    {
+        await container.PatchItemAsync<T>(id, new PartitionKey(partitionKey), patchOperations);
+    }
+
+    public static async Task DeleteItem<T>(this Container container, string id, int partitionKey)
+    {
+        await container.DeleteItemAsync<T>(id, new PartitionKey(partitionKey));
+    }
+
+    public static async Task ReadContainer(this Container container)
+        => await container.ReadContainerAsync();
+
+    public static T Deserialize<T>(this string json)
+    {
+        var jsonSerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        return JsonSerializer.Deserialize<T>(json, jsonSerOptions)!;
+    }
+}
